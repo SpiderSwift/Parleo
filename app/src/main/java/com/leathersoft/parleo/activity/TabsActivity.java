e56639b4ade711d39044085890816e677d703b9a
artem <lionheart66666@mail.ru>
2019-03-17 23:06:59 +0300

    add tab layout, add dialog fragment

---
+++ app/src/main/java/com/leathersoft/parleo/activity/TabsActivity.java
@@ -0,0 +1,45 @@
+package com.leathersoft.parleo.activity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.leathersoft.parleo.R;
+import com.leathersoft.parleo.adapter.MainFragmentsAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TabsActivity extends AppCompatActivity {
+
+    @BindView(R.id.pager_main) ViewPager pager;
+    @BindView(R.id.tab_main) TabLayout tabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabs);
+        ButterKnife.bind(this);
+        pager.setAdapter(new MainFragmentsAdapter(getSupportFragmentManager()));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/leathersoft/parleo/fragment/DialogFragment.java
@@ -0,0 +1,56 @@
+package com.leathersoft.parleo.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.leathersoft.parleo.R;
+import com.leathersoft.parleo.activity.MessagesActivity;
+import com.leathersoft.parleo.messaging.DialogImpl;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.commons.models.IDialog;
+import com.stfalcon.chatkit.dialogs.DialogsList;
+import com.stfalcon.chatkit.dialogs.DialogsListAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class DialogFragment extends Fragment {
+
+    @BindView(R.id.list_dialog)
+    DialogsList dialogsList;
+
+    public DialogFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dialog, container, false);
+        ButterKnife.bind(this, v);
+        DialogsListAdapter adapter = new DialogsListAdapter<DialogImpl>(new ImageLoader() {
+            @Override
+            public void loadImage(ImageView imageView, @Nullable String url, @Nullable Object payload) {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_events));
+            }
+        });
+        adapter.addItem(new DialogImpl());
+        dialogsList.setAdapter(adapter);
+        adapter.setOnDialogClickListener(new DialogsListAdapter.OnDialogClickListener() {
+            @Override
+            public void onDialogClick(IDialog dialog) {
+                startActivity(new Intent(getActivity().getApplicationContext(), MessagesActivity.class));
+            }
+        });
+        return v;
+    }
+
+
+}
