1e57855571a4eac2150b5a1832b21237a36face2
artem <lionheart66666@mail.ru>
2019-03-31 20:15:02 +0300

    update popupwindow

--- app/src/main/java/com/leathersoft/parleo/activity/RegistrationContinueActivity.java
+++ app/src/main/java/com/leathersoft/parleo/activity/RegistrationContinueActivity.java
@@ -10,19 +10,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.view.menu.MenuPopupHelper;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.PopupWindow;
 
 import com.leathersoft.parleo.R;
+import com.leathersoft.parleo.adapter.InterestsAdapter;
+import com.leathersoft.parleo.messaging.Interest;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,13 +50,32 @@ public class RegistrationContinueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration_continue);
         ButterKnife.bind(this);
         //MenuInflater inflater = getMenuInflater();
-        window = new PopupWindow(getApplicationContext());
 
-        View v = getLayoutInflater().inflate(R.layout.activity_login, null);
+
+        View v = getLayoutInflater().inflate(R.layout.window_list, null);
+        window = new PopupWindow(v, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        window.setElevation(1000);
+        RecyclerView recyclerView = v.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        InterestsAdapter adapter = new InterestsAdapter();
+        recyclerView.setAdapter(adapter);
+        List<Interest> interests = new ArrayList<>();
+        interests.add(new Interest("hello", getDrawable(R.drawable.billy)));
+        interests.add(new Interest("hello2", getDrawable(R.drawable.billy)));
+        interests.add(new Interest("hello3", getDrawable(R.drawable.billy)));
+        adapter.setInterests(interests);
+        Button b = v.findViewById(R.id.btn_done);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                window.dismiss();
+            }
+        });
         //v.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.popup_animation));
-        window.setWidth(ConstraintLayout.LayoutParams.MATCH_PARENT);
-        window.setHeight(ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        window.setContentView(v);
+//        window.setWidth(ConstraintLayout.LayoutParams.MATCH_PARENT);
+//        window.setHeight(ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//        window.setContentView(v);
 
 
     }
@@ -70,6 +97,7 @@ public class RegistrationContinueActivity extends AppCompatActivity {
 
     @OnClick(R.id.layout)
     public void close() {
+        window.setAnimationStyle(R.anim.popup_animation);
         window.dismiss();
     }
 
