3b0fd50a0ae5a79e8f621e334f99ff627ba28ef0
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2019-04-02 12:20:14 +0300

    Add MyProfile fragment + add backstack to fragments

--- app/src/main/java/com/leathersoft/parleo/fragment/users/UserAdapter.java
+++ app/src/main/java/com/leathersoft/parleo/fragment/users/UserAdapter.java
@@ -1,19 +1,52 @@
 package com.leathersoft.parleo.fragment.users;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.leathersoft.parleo.R;
+import com.leathersoft.parleo.activity.ProfileActivity;
 import com.leathersoft.parleo.activity.events.EventViewHolder;
+import com.leathersoft.parleo.fragment.ProfileFragment;
 
 public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+
+    private FragmentManager mFragmentManager;
+
+    public UserAdapter(FragmentManager fragmentManager) {
+        mFragmentManager = fragmentManager;
+    }
 
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = View.inflate(viewGroup.getContext(),R.layout.user_item_view,null);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Fragment fragment = new ProfileFragment();
+
+                mFragmentManager.beginTransaction()
+                        .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        .replace(R.id.fragment_container,fragment)
+                        .addToBackStack(null)
+                        .commit();
+
+//                v.getContext().startActivity(
+//                        new Intent(v.getContext(), ProfileActivity.class)
+//                );
+
+
+            }
+        });
         return new UserViewHolder(view);
     }
 
--- app/src/main/java/com/leathersoft/parleo/fragment/users/UserViewHolder.java
+++ app/src/main/java/com/leathersoft/parleo/fragment/users/UserViewHolder.java
@@ -1,31 +1,13 @@
 package com.leathersoft.parleo.fragment.users;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
-
-import com.leathersoft.parleo.R;
-import com.leathersoft.parleo.activity.ProfileActivity;
 
 public class UserViewHolder extends RecyclerView.ViewHolder {
-    TextView mTextView;
+//    TextView mTextView;
 
     public UserViewHolder(@NonNull View itemView) {
         super(itemView);
-        mTextView = itemView.findViewById(R.id.user_card_profile_btn);
-        mTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v,
-                "Profile button pressed",
-                Snackbar.LENGTH_LONG).show();
-                v.getContext().startActivity(
-                        new Intent(v.getContext(),ProfileActivity.class)
-                );
-            }
-        });
     }
 }
