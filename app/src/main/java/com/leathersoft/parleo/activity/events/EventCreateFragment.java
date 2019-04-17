9cf10aa18e62f3ee099a31020695f398c8fbaa46
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2019-04-17 14:01:04 +0300

    [fragments] Fixed event filter

--- app/src/main/java/com/leathersoft/parleo/activity/events/EventCreateFragment.java
+++ app/src/main/java/com/leathersoft/parleo/activity/events/EventCreateFragment.java
@@ -4,19 +4,43 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.leathersoft.parleo.R;
 
 public class EventCreateFragment extends Fragment {
 
+    @BindView(R.id.te_event_descriprion)
+    EditText etDescription;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_event_create,container,false);
+        ButterKnife.bind(this,v);
+
+        etDescription.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.hasFocus()) {
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    switch (event.getAction() & MotionEvent.ACTION_MASK){
+                        case MotionEvent.ACTION_SCROLL:
+                            v.getParent().requestDisallowInterceptTouchEvent(false);
+                            return true;
+                    }
+                }
+                return false;
+            }
+        });
+
         return v;
     }
 
--- app/src/main/java/com/leathersoft/parleo/fragment/FilterEventFragment.java
+++ app/src/main/java/com/leathersoft/parleo/fragment/FilterEventFragment.java
@@ -2,15 +2,22 @@ package com.leathersoft.parleo.fragment;
 
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
+import com.google.android.material.snackbar.Snackbar;
 import com.leathersoft.parleo.R;
 import com.leathersoft.parleo.adapter.InterestsAdapter;
 import com.leathersoft.parleo.messaging.Interest;
@@ -27,6 +34,28 @@ public class FilterEventFragment extends BaseFragment {
     @BindView(R.id.tv_min) TextView min;
     @BindView(R.id.tv_max) TextView max;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.event_filter_menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.event_filter_save:
+                Snackbar.make(getView(),"TODO save event",Snackbar.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
